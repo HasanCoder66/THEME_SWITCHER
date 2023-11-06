de25427d3d8984520bee7af87f886c9fa86aae41
66CoderHasan <hasanashraf874@gmail.com>
2023-11-06 18:43:27 +0500

    Theme switcher done

---
+++ src/components/Card.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="/">
+        <img
+          className="p-8 rounded-t-lg"
+          src="https://scontent.fkhi4-2.fna.fbcdn.net/v/t39.30808-6/396199148_856108126102706_5721945219804232938_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGtwqCtDsBN-UO-Qx5DTp106nzycmJMQg7qfPJyYkxCDhJl4BcvJFNPL8d5YgNU7LZmcG0iYQmmISAIklzJlRGL&_nc_ohc=-PR8lPiCheUAX8kUDhA&_nc_zt=23&_nc_ht=scontent.fkhi4-2.fna&oh=00_AfCyqJEGVAEwe0aMRuAZlq1MMNqR-MTiAwCYkgZhXj8zNw&oe=65456B5D"
+          alt="product_image1"
+        />
+      </a>
+      <div className="px-5 pb-5">
+        <a href="/">
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          MERN Stack Developer crafting web magic, inviting you to explore full-stack development together.
+          </h5>
+        </a>
+        <div className="flex items-center mt-2.5 mb-5">
+          <svg
+            className="w-4 h-4 text-yellow-300 mr-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-300 mr-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-300 mr-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-yellow-300 mr-1"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <svg
+            className="w-4 h-4 text-gray-200 dark:text-gray-600"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            4.0
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            $599
+          </span>
+          <a
+            href="/"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+           Place Your Order
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/context/theme.js
@@ -0,0 +1,14 @@
+import { useContext , createContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode : 'light',
+    darkTheme : () => {},
+    lightTheme : () => {}
+})
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+export function useTheme () {
+    return useContext(ThemeContext)
+}
